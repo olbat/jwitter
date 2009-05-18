9c060d7a2cc1584311ae9be409a9dfabee49bada
Luc Sarzyniec <luc.sarzyniec@gmail.com>
2009-05-18 17:44:16 +0000

    Add new quotes.

--- Jwitter/src/java/jwitter/Quote.java
+++ Jwitter/src/java/jwitter/Quote.java
@@ -17,7 +17,15 @@ public class Quote {
         "Show me your tongue !",
         "VisualWebJSF is a fucking crap !",
         "Might I ask you where do you keep the SPOOOOONS ?",
-        "Hubert Cumberdale, you're tasting like soot and poo !"};
+        "Hubert Cumberdale you taste like soot and poo !",
+        "I thought you were out fighting the great war",
+        "JSP Owns! Erm, or not ...",
+        "I'm ugly",
+        "I like rusty spoons",
+        "The feeling of rust ... is almost orgasmic.",
+        "Hello there little child",
+        "I'm here to enquire about your spoons",
+        "I wonder what you taste like"};
 
     public static String getRandomQuote() {
         return quotes[(int)(Math.random() * quotes.length)];
